c414677f6914a07b599cc6b077c502dcbc02342a
kamiaki <247557768@qq.com>
2021-08-24 10:04:37 +0800

    在交换机上 加入一个新的 队列web 供前端测试用

--- src/main/java/com/aki/rabbitmqtest/config/RabbitConfig.java
+++ src/main/java/com/aki/rabbitmqtest/config/RabbitConfig.java
@@ -1,12 +1,10 @@
 package com.aki.rabbitmqtest.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -49,6 +47,36 @@ public class RabbitConfig {
     Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("");
     }
+
+
+//    /**
+//     * 创建队列
+//     * @return
+//     */
+//    @Bean("testQueue")
+//    Queue testQueue() {
+//        return new Queue(Config.TEST_QUEUE,false);
+//    }
+//
+//    /**
+//     * 创建信道
+//     * @return
+//     */
+//    @Bean("testExchange")
+//    FanoutExchange testExchange() {
+//        return new FanoutExchange(Config.TEST_EXCHANGE);
+//    }
+//
+//    /**
+//     * 绑定队列和信道
+//     * @return
+//     */
+//    @Bean
+//    Binding binding(@Qualifier(value = "testQueue") Queue testQueue,
+//                    @Qualifier(value = "testExchange")  FanoutExchange testExchange) {
+//        return BindingBuilder.bind(testQueue).to(testExchange);
+//    }
+
 
 //    /**
 //     * 绑定队列和他指定的适配器
